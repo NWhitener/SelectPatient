f6e335ded4ee9751472b3651d91d7792449ad251
nathanwhitener <whitennp@wfu.edu>
2022-03-31 20:45:28 -0400

    config support

--- run_ffnn.py
+++ run_ffnn.py
@@ -1,8 +1,8 @@
-from click import option
+
 import preprocess as ps 
 import ffnn as ffnn 
 
 def full_model(data, labels):
     labels_patient, data_patient, num_labels = ps.selectPatient(labels, data, option = False, balance=True, PrepareData= True)
     ffnn.run_ffnn_model(data_patient,labels_patient, num_labels)
-    
+
