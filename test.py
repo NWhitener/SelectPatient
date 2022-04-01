101e94e0b2ad009448c229eb7a627f33d2fc2fc5
nathanwhitener <whitennp@wfu.edu>
2022-03-31 20:43:04 -0400

    Config support

--- test.py
+++ test.py
@@ -1,10 +1,11 @@
 import preprocess as ps 
 import ffnn as ffnn 
 from config import Config
+import run_ffnn 
 
 def main(yaml_file_name):
   cfg = Config(yaml_file_name)
-
+  run_ffnn.full_model(cfg)
 
 if __name__ == "__main__":
   #config_file = '../yaml/config.yaml'
